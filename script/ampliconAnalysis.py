27978bdb38ec1dbc75654d12ebc2b95612b69235
elhb <eriklhb@gmail.com>
2013-04-02 13:38:49 +0200

    created new file for analysis of DR amplicon project

---
+++ script/ampliconAnalysis.py
@@ -0,0 +1,18 @@
+#! /bin/env python
+#! /usr/bin/env python2.7
+
+MASTER = os.getppid()
+
+def main():
+#--------------------------MAIN PROGRAM-----------------------------
+
+#--------------------------MAIN PROGRAM END-------------------------
+
+#--------------------- Functions, Subroutines and classes --------------------	
+
+#####
+#check if run or imported // call main() or not
+#####
+if __name__ == "__main__":
+    main()
+#END of script
