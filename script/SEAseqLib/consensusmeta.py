ad31421675ec232b86129c1776d5fa078eb473b1
elhb <eriklhb@gmail.com>
2013-09-10 11:56:32 +0200

    added some headers to output

--- script/SEAseqLib/consensusmeta.py
+++ script/SEAseqLib/consensusmeta.py
@@ -219,8 +219,11 @@ def foreachcluster_meta(cluster_pairs):
             output += str(cluster.definedampliconcount)+' amplicon(s) have enough data (>=1 cons with >= '+str(indata.minimum_support)+'% support and >= '+str(indata.minimum_reads)+' reads):\n'
             output += perAmpOut + '\n'
             output += '# Details:\n'
+            output += '# Alignemnts:\n'
             output += aligmnmentsOut + '\n'
+            output += '# Pairs:\n'
             output += pairsOut + '\n'
+            output += '# Original cdhit data:\n'
             output += filesOut + '\n'
 
 	return [output, cluster]
